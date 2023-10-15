3f7b49792b9d594364abe25869d04ec0fe555594
daumi <140564095+DaumiJunior@users.noreply.github.com>
2023-10-15 01:32:18 -0300

    first commit

---
+++ Daumi/src/main/java/me/dio/Daumi/DaumiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Daumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaumiApplication.class, args);
+	}
+
+}
---
+++ Daumi/src/test/java/me/dio/Daumi/DaumiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Daumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
